7024634a20ce553dc597d5ef97192db5f45a1fef
ABHI-PC\Abhi Lapsiwala <abhicodes.16@gmail.com>
2021-05-01 00:14:14 +0530

    cors commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var multer = require('multer');
+var cors = require('cors')
 var upload = multer();
 require("./db/conn");
 const userRouter = require("./routers/user");
@@ -9,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 4000
 
 app.use(express.json());
+app.use(cors())
 app.use(upload.array()); 
 app.use(userRouter);
 app.use(authRouter);
