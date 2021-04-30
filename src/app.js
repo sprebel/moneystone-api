f9e6d3450ab63f18faeaeaf7720c0432f1761fb4
abhi_softieons <abhi.softieons@gmail.com>
2021-04-30 17:07:20 +0530

    profile - update commit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ var upload = multer();
 require("./db/conn");
 const userRouter = require("./routers/user");
 const authRouter = require("./routers/auth");
+const profileRouter = require("./routers/profile");
 
 const app = express();
 const port = process.env.PORT || 4000
@@ -12,5 +13,6 @@ app.use(express.json());
 app.use(upload.array()); 
 app.use(userRouter);
 app.use(authRouter);
+app.use(profileRouter);
 
 app.listen(port, () => console.log(`connection is setup at ${port}`));
