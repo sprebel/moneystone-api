03d3160e2af34f3f6da7073056436c002b33934d
Deadlycodes <85147054+Deadlycodes@users.noreply.github.com>
2021-06-01 18:24:17 +0530

    Update app.js
    change port

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const invite = require("./routers/invite");
 const appVersion = require("./routers/appVersion");
 
 const app = express();
-var serverPort = 8080;
+var serverPort = 80;
 const port = process.env.PORT || serverPort
 
 app.use(express.json());
